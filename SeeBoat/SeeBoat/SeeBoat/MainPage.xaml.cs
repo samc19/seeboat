6d0fcdd201d7ebe5cdf0503219859e51a0937e03
Talia Spitz <v-taspit@microsoft.com>
2018-07-19 11:31:40 -0400

    Changed name from MapApp to SeeBoat

---
+++ SeeBoat/SeeBoat/SeeBoat/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Maps;
+using Plugin.Geolocator;
+
+namespace SeeBoat
+{
+	public partial class MainPage : ContentPage
+	{
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        //button is clicked to create map
+        private void GetMap_Clicked(object sender, EventArgs e)
+        {
+            GenerateMapAroundLocation();
+        }
+
+        //creates new MapPage
+        async void GenerateMapAroundLocation()
+        {
+            await Navigation.PushAsync(new MapPage());
+        }
+        async void AboutItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new About());
+        }
+
+        private void MapItem_Clicked(object sender, EventArgs e)
+        {
+            GenerateMapAroundLocation();
+        }
+    }
+	}
+
