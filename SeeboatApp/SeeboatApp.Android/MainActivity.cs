2cc9b86fa352f697444fdcba2d6de74d3c64d0eb
Talia Spitz <v-taspit@microsoft.com>
2018-07-23 14:10:54 -0400

    App will now request permissions.

--- SeeboatApp/SeeboatApp.Android/MainActivity.cs
+++ SeeboatApp/SeeboatApp.Android/MainActivity.cs
@@ -6,12 +6,23 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.Content;
+using Android;
+using Android.Support.V4.App;
 
 namespace SeeboatApp.Droid
 {
     [Activity(Label = "SeeboatApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback
     {
+        string[] PermissionsLocation =
+        {
+            Manifest.Permission.AccessCoarseLocation,
+            Manifest.Permission.AccessFineLocation
+        };
+
+        const int RequestLocationId = 0;
+
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -20,7 +31,16 @@ namespace SeeboatApp.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            CheckPermissions();
             LoadApplication(new App());
+        }
+
+        private void CheckPermissions()
+        {
+            if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessCoarseLocation) != (int)Permission.Granted | ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != (int)Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, PermissionsLocation, RequestLocationId);
+            }
         }
     }
 }
