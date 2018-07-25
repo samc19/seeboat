ce78b1afec83b8885c540398de2432591a238af5
Talia Spitz <v-taspit@microsoft.com>
2018-07-25 15:43:30 -0400

    Linked to Laura's git/website and changed icons.

--- SeeboatApp/SeeboatApp/About.xaml.cs
+++ SeeboatApp/SeeboatApp/About.xaml.cs
@@ -1,20 +1,36 @@
-﻿using System;
+﻿using Android.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SeeboatApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class About : ContentPage
-	{
-		public About ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class About : ContentPage
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        public ICommand ClickCommand => new Command<string>((url) =>
+        {
+            Device.OpenUri(new System.Uri(url));
+        });
+
+        private void GitHub_Clicked(object sender, EventArgs e)
+        { 
+            Device.OpenUri(new System.Uri("https://github.com/lperovich/seeboat/"));
+        }
+
+        private void LauraWebsite_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new System.Uri("http://www.lauraperovich.com/projects/seeBoat.html"));
+        }
+    }
 }
